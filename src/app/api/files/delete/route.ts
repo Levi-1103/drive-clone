7bf8fbf1e4a53e9033a332d43817284582963695
Levi-1103 <58707841+Levi-1103@users.noreply.github.com>
2025-03-13 05:25:27 +0000

    remove massive api route comment

--- src/app/api/files/delete/route.ts
+++ src/app/api/files/delete/route.ts
@@ -4,40 +4,6 @@ import { env } from "@/env/server";
 import { DeleteObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { NextResponse } from "next/server";
 
-// export const DELETE = auth(async function DELETE(req) {
-//     if (!req.auth) return NextResponse.json({ message: "Not authenticated" }, { status: 401 })
-
-//     const { searchParams } = new URL(req.url);
-
-//     const key = searchParams.get("key")
-
-//     if (!key) return NextResponse.json({ message: "Bad Request" }, { status: 400 })
-
-//     try {
-//         const client = new S3Client({
-//             region: env.S3_REGION,
-//             credentials: {
-//                 accessKeyId: env.S3_ACCESS_KEY_ID,
-//                 secretAccessKey: env.S3_SECRET_ACCESS_KEY
-//             },
-//             endpoint: env.S3_ENDPOINT,
-//             forcePathStyle: true,
-//         })
-
-
-//         const deleteCommand = await client.send(new DeleteObjectCommand({ Bucket: env.S3_NAME, Key: key }));
-
-//         await deleteFile(key, req.auth.user?.id!)
-
-
-//         return NextResponse.json(deleteCommand)
-//     } catch (error) {
-//         console.log(error)
-//         return NextResponse.json({ error: error.message })
-//     }
-
-// });
-
 export async function DELETE(req: Request) {
 
     const session = await auth();
